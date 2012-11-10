7e4c87c4c38c599c5c0931329bb9b640665087fd
CorwiN <corlinus@gmail.com>
2012-11-10 18:15:12 +0400

    added install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# script to install config files
+# usage: install.sh file [file2]...
+
+for file in $*
+do
+    dst_file=$HOME/.$file
+    if (test -f $dst_file) then
+        if (test -h $dst_file) then
+            echo "$dst_file already exists and it's a link"
+        else    
+            echo "$dst_file already exists"
+        fi
+    else
+        if (ln -s $(pwd)/$file $dst_file) then
+            echo "$dst_file successfully installed"
+        fi
+    fi
+done
+echo
+exit 0
